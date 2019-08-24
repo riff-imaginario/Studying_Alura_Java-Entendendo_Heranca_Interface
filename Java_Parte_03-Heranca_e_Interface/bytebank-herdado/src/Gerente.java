a254551c43b5277d7eff9c11b4ca7e6bfef5b284
riff-imaginario <lucas.aleph.101@gmail.com>
2019-08-24 16:14:50 -0300

    add override getBonificacao()

---
+++ Java_Parte_03-Heranca_e_Interface/bytebank-herdado/src/Gerente.java
@@ -0,0 +1,21 @@
+
+public class Gerente extends Funcionario {
+
+	private int senha;
+	
+	public void setSenha(int senha) {
+		this.senha = senha;
+	}
+	
+	public boolean autentica(int senha) {
+		if(this.senha == senha)
+			return true;
+		return false;
+	}
+	
+	// Reescrita do getBonificacao da classe Funcionario
+	// Reescrita: Mesma assinatura
+	public double getBonificacao() {
+		return super.salario;
+	}
+}
